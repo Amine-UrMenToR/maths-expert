d0394e973298b3dc772674b5a7ce41f110fe029f
Amine-UrMenToR <houari.amine30@gmail.com>
2026-03-15 01:07:13 +0000

    Fix CORS policy to allow frontend requests

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], allowedHeaders: ['Content-Type', 'Authorization'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
